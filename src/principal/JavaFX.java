d92db3506b22ba829c1a38fc1f282be8eb22cd79
Julien121 <julien121.clement@gmail.com>
2015-12-28 19:49:12 +0100

    0.1

--- src/principal/JavaFX.java
+++ src/principal/JavaFX.java
@@ -1,5 +1,17 @@
 package principal;
 
-public class JavaFX {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import vue.VueMorse;
 
+public class JavaFX extends Application {
+	
+    public static void main(String[] args) {
+    	launch(args);
+    }
+
+	@Override
+	public void start(Stage primaryStage){
+		new VueMorse(primaryStage);
+	}
 }
--- src/vue/VueMorse.java
+++ src/vue/VueMorse.java
@@ -1,5 +1,25 @@
 package vue;
 
-public class VueMorse {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class VueMorse extends Scene implements EventHandler<ActionEvent> {
+	
+	private Stage primaryStage;
+	
+	public VueMorse(Stage stage) {
+
+		super(new Group(), 400, 600);
+
+
+		this.primaryStage = stage;
+	}
+
+	@Override
+	public void handle(ActionEvent arg0) {
+		
+	}
 }
