ce50869d2496e752276e0a2fe49c41ff944c84c9
Julien121 <julien121.clement@gmail.com>
2015-12-28 20:38:14 +0100

    0.1

--- src/controleur/CtrlVueMorse.java
+++ src/controleur/CtrlVueMorse.java
@@ -2,19 +2,25 @@ package controleur;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import vue.VueMorse;
 
-public class CtrlVueMorse implements EventHandler<ActionEvent> {
+public class CtrlVueMorse{
 	private VueMorse vue;
 	
+	@FXML
+	private TextArea tAConvert;
+	
+	public void setModele() {
+		
+	}
 	public void setVue(VueMorse vueMorse) {
 		this.vue = vue;
 	}
-
-	@Override
-	public void handle(ActionEvent event) {
-		// TODO Auto-generated method stub
-		
+	public void handleActionConvert(ActionEvent event) {
+		System.out.println(event.getSource().toString());
+		System.out.println(this.tAConvert.getText());
 	}
 
 }
