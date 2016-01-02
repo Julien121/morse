90a7e3a0fe583d156245b30882cccc0e708a289c
Julien121 <julien121.clement@gmail.com>
2016-01-02 16:28:05 +0100

    0.1

--- src/perso/utils/MonJavaFX.java
+++ src/perso/utils/MonJavaFX.java
@@ -5,6 +5,11 @@ import javafx.scene.control.Alert.AlertType;
 
 public class MonJavaFX {
 	
+	/**
+	 * Affiche une fenêtre d'erreur
+	 * @param header Le titre de la fenêtre
+	 * @param msg Le message d'erreur
+	 */
 	public static void erreur(String header, String msg) {
 		
 		Alert dialogE = new Alert(AlertType.ERROR);
@@ -14,7 +19,11 @@ public class MonJavaFX {
 		dialogE.showAndWait();
 		
 	}
-
+	/**
+	 * Affiche une fenêtre informationnelle
+	 * @param header Le titre de la fenêtre
+	 * @param msg Le message d'informations
+	 */
 	public static void info(String header, String msg) {
 		
 		Alert dialogE = new Alert(AlertType.INFORMATION);
